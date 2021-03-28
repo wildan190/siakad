17c99368a1b8a12e9fd75518002c134ccc7ef21e
Wildan Belfiore <mikewildan@outlook.com>
2021-03-28 13:24:31 +0700

    'master'

--- tmhs.sql
+++ tmhs.sql
@@ -1,23 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 10 Mei 2020 pada 02.12
--- Versi Server: 5.6.21
--- PHP Version: 5.6.3
+-- Waktu pembuatan: 28 Mar 2021 pada 08.23
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dblatihan`
+-- Database: `pwe`
 --
 
 -- --------------------------------------------------------
@@ -26,43 +27,38 @@ SET time_zone = "+00:00";
 -- Struktur dari tabel `tmhs`
 --
 
-CREATE TABLE IF NOT EXISTS `tmhs` (
-`id_mhs` int(11) NOT NULL,
-  `nim` varchar(9) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `alamat` varchar(128) NOT NULL,
-  `prodi` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `tmhs`
---
-
-INSERT INTO `tmhs` (`id_mhs`, `nim`, `nama`, `alamat`, `prodi`) VALUES
-(1, '15.50.001', 'Muhammad Hafid', 'Tarakan, Kalimantan Utara', 'S1 - SI'),
-(2, '15.51.002', 'Muhammad Uwais', 'Juata Laut, Tarakan', 'S1 - TI'),
-(3, '15.30.001', 'Indrianti bin Mansur', 'Lingkas Ujung, Tarakan', 'D3 - MI'),
-(6, '19.51.005', 'Muhammad Baba', 'Tanjung Selor', 'S1 - TI');
+CREATE TABLE `tmhs` (
+  `id_tmhs` int(11) NOT NULL,
+  `kd_mkul` varchar(11) NOT NULL,
+  `nama_mkul` varchar(30) NOT NULL,
+  `kd_dosen` varchar(30) NOT NULL,
+  `jam` varchar(11) NOT NULL,
+  `ruang_kelas` varchar(11) NOT NULL,
+  `jumlah_mhs` int(11) NOT NULL,
+  `tanggal_mulai` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tmhs`
+-- Indeks untuk tabel `tmhs`
 --
 ALTER TABLE `tmhs`
- ADD PRIMARY KEY (`id_mhs`);
+  ADD PRIMARY KEY (`id_tmhs`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tmhs`
+-- AUTO_INCREMENT untuk tabel `tmhs`
 --
 ALTER TABLE `tmhs`
-MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id_tmhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
